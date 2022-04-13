6051b0e8d035c7f7d5aa8c41e98e624cce0962d9
SadMeercat <igor9823@gmail.com>
2022-04-13 16:01:05 +0300

    add the second option

--- HomeWork_01/Program.cs
+++ HomeWork_01/Program.cs
@@ -1,6 +1,11 @@
 ﻿Console.Write("Number: ");
 string number = "";
 number = Console.ReadLine();
-
+//First option
 string lastNumber = Convert.ToString(number[number.Length-1]);
 Console.WriteLine(lastNumber);
+
+//Second option
+
+int resultNum = Convert.ToInt32(number)%10;
+Console.WriteLine(resultNum);

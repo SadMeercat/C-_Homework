d0118a843cc3e202207037543ec5408de7c0a36b
SadMeercat <igor9823@gmail.com>
2022-05-02 17:05:39 +0300

    update 4 homework

--- Homework_04/Program.cs
+++ Homework_04/Program.cs
@@ -37,7 +37,6 @@ Console.WriteLine("Сумма цифр: " + _summ);
 
 Console.Write("Введите числа через запятую: ");
 string _numbers = Console.ReadLine();
-_numbers = _numbers.Replace(" ", "");
 string[] _arrayNumbers = _numbers.Split(new char[] {','});
 
 for(int i = 0; i < _arrayNumbers.Length; i++){
